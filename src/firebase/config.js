84c1b52e6dba73e37472773090572b00bbb01eb7
Gonzalo Almiron <gonzaloalmiron.25@gmail.com>
2023-03-17 12:43:12 -0300

    d

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,18 +1,18 @@
-import { initializeApp } from "firebase/app";
-import {getFirestore} from "firebase/firestore"
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// import { initializeApp } from "firebase/app";
+// import {getFirestore} from "firebase/firestore"
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyDlUZzxJ50LvS3SGEGYaHgyccCxBYRPY_s",
-  authDomain: "dcv-app.firebaseapp.com",
-  projectId: "dcv-app",
-  storageBucket: "dcv-app.appspot.com",
-  messagingSenderId: "665392554907",
-  appId: "1:665392554907:web:6e6eb419782e54b936b73e"
-};
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDlUZzxJ50LvS3SGEGYaHgyccCxBYRPY_s",
+//   authDomain: "dcv-app.firebaseapp.com",
+//   projectId: "dcv-app",
+//   storageBucket: "dcv-app.appspot.com",
+//   messagingSenderId: "665392554907",
+//   appId: "1:665392554907:web:6e6eb419782e54b936b73e"
+// };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app)
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// export const db = getFirestore(app)
